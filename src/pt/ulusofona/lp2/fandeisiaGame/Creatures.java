d5e292e0cb14f20701f3679283ef9014e5ca43fc
David Silva <a21904353@alunos.ulht.pt>
2019-11-13 19:09:40 +0000

    update dos methods

--- src/pt/ulusofona/lp2/fandeisiaGame/Creatures.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/Creatures.java
@@ -14,6 +14,39 @@ public class Creatures {
         this.tipo = tipo;
     }
 
-    //ToString talvez...
-    //Geters talvez...
+    public int getId(){
+        /* Deve devolver o ID da criatura.*/
+        return 999;
+    }
+
+    public String getImagePNG(){
+        /*Deve devolver o nome do ficheiro de imagem
+         * (formato PNG) que representa a criatura.
+         *
+         * (As imagens a usar devem ser colocadas na pasta
+         * src/images e devem ter tamanho 50x50. As imagens
+         * devem ter fundo transparente para que se consiga
+         * ver se estão num quadrado branco ou preto).
+         *
+         * Caso os alunos não pretendam definir
+         * nenhuma imagem, a função pode
+         * simplesmente retornar ​null​. Isto fará com
+         * que o visualizador use uma imagem
+         * pré-definida por omissão.
+         * */
+        return "incompleto";
+    }
+
+    public String toString(){
+        /*
+         * Retorna uma ​String​ com a informação sobre a criatura.
+         *
+         * Sintaxe​:
+         * “<ID> | <Tipo> | <ID Equipa> | <Nr Pontos> @ (<x>, <y>) <Orientacão>”
+         *
+         * Onde <Nr Pontos> corresponde a quantidade
+         * de Tesouros encontrados pela criatura.
+         * */
+        return "toString incompleto";
+    }
 }
