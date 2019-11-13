021b268dd162ddf4eb38af46f441fb7d5335d547
Andre-21804814 <56220716+Andre-21804814@users.noreply.github.com>
2019-11-13 18:43:31 +0000

    Add files via upload

---
+++ src/pt/ulusofona/lp2/fandeisiaGame/Criaturas.java
@@ -0,0 +1,19 @@
+package pt.ulusofona.lp2.fandeisiaGame;
+
+public class Criaturas {
+
+    int id;
+    String tipo;
+    int posX;
+    int posY;
+    String orientação;
+
+    public Criaturas(int id, String tipo, String orientação){
+        this.id = id;
+        this.orientação = orientação;
+        this.tipo = tipo;
+    }
+
+    //ToString talvez...
+    //Geters talvez...
+}
