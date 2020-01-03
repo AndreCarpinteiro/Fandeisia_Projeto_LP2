7a2a50120a52869b2f96e4cbf2f144dc9698af40
André Carpinteiro 21804814 <andre.98leandro@gmail.com>
2020-01-03 04:58:56 +0000

    Fixes code quality e jUnit marado

--- src/pt/ulusofona/lp2/fandeisiaGame/ComparadorId.java
+++ src/pt/ulusofona/lp2/fandeisiaGame/ComparadorId.java
@@ -4,8 +4,8 @@ import java.util.Comparator;
 
 public class ComparadorId implements Comparator<Creature> {
     public int compare(Creature o1, Creature o2) {
-        if (o1.getId() < o2.getId()) return -1;
-        else if (o1.getId() > o2.getId()) return +1;
-        else return 0;
+        if (o1.getId() < o2.getId()){ return -1;}
+        else if (o1.getId() > o2.getId()){ return +1;}
+        else {return 0;}
     }
 }
